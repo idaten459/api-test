4c98032ad6765793005cb6c1a8872eb8afdc279d
idaten <shukai2013@gmail.com>
2019-12-11 11:26:17 +0900

    exsamples 200,403

--- get_request.js
+++ get_request.js
@@ -6,10 +6,10 @@ function myFunction() {
         "https://kenkoooo.com/atcoder/atcoder-api/v2/user_info?user=kenkoooo",
         "https://kenkoooo.com/atcoder/atcoder-api/results?user=wata"
     ];
-    var response = UrlFetchApp.fetch(url[0]); // 200
-    var response = UrlFetchApp.fetch(url[1]); // 200
-    var response = UrlFetchApp.fetch(url[2]); // 200
+    // var response = UrlFetchApp.fetch(url[0]); // 200
+    // var response = UrlFetchApp.fetch(url[1]); // 200
+    // var response = UrlFetchApp.fetch(url[2]); // 200
     var response = UrlFetchApp.fetch(url[3]); // 403
-    var response = UrlFetchApp.fetch(url[4]); // 403
+    // var response = UrlFetchApp.fetch(url[4]); // 403
     Logger.log(response);
 }
